25cebaf503ffef8075d1e003e5e484f263627b02
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2023-07-17 23:02:18 +0700

    Unions type

---
+++ 3-basic-unions-type.ts
@@ -0,0 +1,8 @@
+// Ex-1.Add type for user with gender is one of "male", "female", "unknown"
+const user = {
+	name: "Huy dep chai",
+	age: 18,
+	gender: "unknown"
+}
+
+// Ex-2. Update Ex-1 to make gender accept value of "male", "female", "unknown" or any string
