08ce13ff7554851ae5dc0d1773e82120ed3d5854
Truong <truong.vinh.trinh@mgm-tp.com>
2023-07-20 12:54:38 +0700

    Basic record type

---
+++ 5-basic-record-type.ts
@@ -0,0 +1,18 @@
+// Resolve the type problem
+const createCache = () => {
+	const cache = {};
+
+	const add = (id: string, value: string) => {
+		cache[id] = value;
+	};
+
+	const remove = (id: string) => {
+		delete cache[id];
+	};
+
+	return {
+		cache,
+		add,
+		remove,
+	};
+};
